f91479321849dfec7a50c188de7ee25c69803450
rodrigo1995vav <rodrigo1995val@hotmail.com>
2022-04-27 18:55:39 -0300

    create transactions model

---
+++ models/transactions.js
@@ -0,0 +1,23 @@
+const { sequelize } = require("../../config/postgres");
+const { DataTypes } = require("sequelize");
+
+const Transactions = sequelize.define(
+  "transactions",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.FLOAT,
+    },
+    description: {
+      type: DataTypes.STRING,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Transactions;
