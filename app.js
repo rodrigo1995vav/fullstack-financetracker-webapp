433eee2e15d7e01ccba81b78d45bd2adebb52253
rodrigo1995vav <rodrigo1995val@hotmail.com>
2022-04-28 14:17:42 -0300

    feat: index for dinamic routes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use("/api", require("./routes/auth"))
+app.use("/api", require("./routes/"))
 
 app.listen (port, () => {
     console.log(`Your app is ready and running on http://localhost:${port}`)
