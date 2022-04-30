226a131edb086344190a168f206f7ad3df7e12b0
rodrigo1995vav <rodrigo1995val@hotmail.com>
2022-04-30 16:44:30 -0300

    add logout controller

---
+++ controllers/logout.js
@@ -0,0 +1,23 @@
+const { refreshModel } = require("../models");
+
+const handleLogout = async (req, res) => {
+    //TODO On client, also delete the accessToken
+
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204); //No content
+    const refreshToken = cookies.jwt;
+
+    // Delete refreshToken in db if found
+    const foundUser = refreshModel.destroy({where:{token:refreshToken}});
+    if (!foundUser) {
+        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+        return res.sendStatus(204);
+    }
+
+    
+
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    res.sendStatus(204);
+}
+
+module.exports =  handleLogout 
