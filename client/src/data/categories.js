21ef0bababa02b887242600086332b940ca52ba3
rodrigo1995vav <rodrigo1995val@hotmail.com>
2022-05-09 03:58:27 -0300

    comment added

--- client/src/data/categories.js
+++ client/src/data/categories.js
@@ -1,1 +1,4 @@
+
+//This should be in the postgre database, since theres no admin dashboard to charge the categories is useless to set a model for this.
+
 export const categories = [{ name: "Food", type: "Expense" }, { name: "Supermaket", type: "Expense" } , {name:"Pay Check", type: "Income"} , {name:"Clothes", type:"Expense"}];
